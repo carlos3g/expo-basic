79b3cad1ffc20fc82ccc0e4ef11b6b5070c47380
carlos3g <carlosmesquita156@gmail.com>
2021-05-20 11:13:28 -0300

    [REFACTOR] yarn remove expo-status-bar

--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 // import Navigation from './Navigation';
-import { StatusBar } from 'expo-status-bar';
 
 import HomeScreen from '@screens/Home';
 
 function App() {
   return (
-    <>
-      <StatusBar translucent />
-      <NavigationContainer>
-        <HomeScreen />
-      </NavigationContainer>
-    </>
+    <NavigationContainer>
+      <HomeScreen />
+    </NavigationContainer>
   );
 }
 
